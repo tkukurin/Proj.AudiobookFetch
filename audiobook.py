450d4e895a2702ea0e75c898c3d0d337cf341e10
toni <tkukurin@gmail.com>
2018-11-20 20:07:40 +0100

    modify to accept full urls

--- audiobook.py
+++ audiobook.py
@@ -2,6 +2,10 @@
 
 import youtube_dl
 import sys
+import requests
+import re
+import itertools as it
+import multiprocessing as mp
 
 
 ydl_opts = {
@@ -12,31 +16,40 @@ ydl_opts = {
         'preferredquality': '192',
     }],
     'download-archive': 'stats',
-    'yes-playlist': True,
 }
 
-
-if len(sys.argv) == 1:
-  print('Usage: audiobook.py [vid_ids]')
-  sys.exit(1)
-
-import requests
-import re
-def find_vids(playlist_url):
-  url = 'https://youtube.com/playlist?list={}'.format(playlist_url)
+def find_vids(playlist_id):
+  url = 'https://youtube.com/playlist?list={}'.format(playlist_id)
   str_ = requests.get(url).text
   return [x[1] for x in re.findall(r'(watch.v=)([^&]*)', str_)]
   
-vids = find_vids(sys.argv[1]) if sys.argv[1].startswith('PL') else sys.argv[1:]
-vids = set(vids)
 
-from multiprocessing import Pool
 def download(v):
   with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download([v])
-  
-with Pool(1) as p:
-  vids = ['http://www.youtube.com/watch?v={}'.format(vid) for vid in vids]
-  p.map(download, vids)
+
+
+def urlfmt(url_or_vid):
+  fmt = 'http://www.youtube.com/watch?v={}'
+  return url_or_vid \
+    if 'youtube.com' in url_or_vid \
+    else fmt.format(url_or_vid)
+
+
+def to_full_url(vid):
+  return find_vids(vid) \
+    if vid.startswith('PL') \
+    else [urlfmt(vid)]
+
+
+if __name__ == '__main__':
+  if len(sys.argv) == 1:
+    print('Usage: audiobook.py [vid_ids]')
+    print('If name starts with PL, assumes playlist')
+    sys.exit(1)
+
+  vids = it.chain(*map(to_full_url, sys.argv[1:]))
+  with mp.Pool(1) as p:
+    p.map(download, set(vids))
 
 
